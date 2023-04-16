ee3ced85e4d2e292f0d707fb1eb9f36339683c0a
sivaram000 <katta.sivaram@suprsend.com>
2023-04-16 16:05:53 +0530

    title change

--- src/InboxForm.js
+++ src/InboxForm.js
@@ -157,7 +157,7 @@ function InboxForm({ showToast, setShowToast }) {
   return (
     <div className="col-span-1">
       <h1 className="text-xl text-black font-bold">
-        React Native Inbox Notification example
+        Inbox Notification example
       </h1>
       <p className="mt-2 text-sm font-normal text-[#4B5563]">
         This is where you can add the content of the notification
